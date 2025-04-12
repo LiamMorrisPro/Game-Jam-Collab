f8698e896293446dce232dcc2da8dece63d1e50e
LiamMorrisPro <LiamMorrisPro@gmail.com>
2025-04-12 14:54:28 -0400

    Adding player controller and rigidbody stuff

---
+++ wild-jam-project/scripts/player_ground_detect.gd
@@ -0,0 +1,13 @@
+extends Area2D
+class_name ground_check_area
+
+#keep list of colliders in ground check area so we can check if we are currently colliding
+var collider_list = []
+
+
+func _on_body_entered(body: Node2D) -> void:
+	collider_list.append(body)
+
+
+func _on_body_exited(body: Node2D) -> void:
+	collider_list.erase(body)
