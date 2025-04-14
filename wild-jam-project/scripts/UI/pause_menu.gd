f801754ecdc75fdfb1d5de4ba8886ebb6673bb4f
Remi <iremideshodipos@gmail.com>
2025-04-14 18:59:45 -0500

    pause menu implematation

--- wild-jam-project/scripts/UI/pause_menu.gd
+++ wild-jam-project/scripts/UI/pause_menu.gd
@@ -3,21 +3,43 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$BlurFade.play("RESET")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	pressedEsc()
 
 
 func _on_resume_pressed() -> void:
-	pass # Replace with function body.
+	resume()
+
+func _on_restart_pressed() -> void:
+	resume()
+	get_tree().reload_current_scene()
 
 
 func _on_main_menu_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://UI/main_menu.tscn")
+
+func _on_levels_pressed() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://UI/level_selector_screen.tscn")
 
 
-func _on_options_pressed() -> void:
-	pass # Replace with function body.
+
+func resume():
+	get_tree().paused = false
+	$BlurFade.play_backwards("blur")
+	
+func pause():
+	get_tree().paused = true
+	$BlurFade.play("blur")
+
+
+func pressedEsc():
+	if Input.is_action_just_pressed("esc") and get_tree().paused == false:
+		pause()
+	elif Input.is_action_just_pressed("esc") and get_tree().paused == true:
+		resume()
