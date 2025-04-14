e0e03cb5d42da07053a3d9476082ba9d935bcac6
LiamMorrisPro <LiamMorrisPro@gmail.com>
2025-04-14 13:07:43 -0400

    removed duplicate dog scene

--- wild-jam-project/scripts/health_component.gd
+++ wild-jam-project/scripts/health_component.gd
@@ -2,6 +2,8 @@ extends Node2D
 class_name HealthComponent
 
 signal dead
+@onready var sprite_2d: Sprite2D = $"../Sprite2D"
+
 
 @export var MAX_HEALTH := 100.0
 var health : float
@@ -11,6 +13,14 @@ func _ready():
 
 func damage(attack):
 	health -= attack
+	
+	if health >= 50:
+		sprite_2d.frame = 39
+	elif health >= 20:
+		sprite_2d.frame = 40
+	elif health >= 20:
+		sprite_2d.frame = 41
+	
 	if health <= 0:
 		died()
 
