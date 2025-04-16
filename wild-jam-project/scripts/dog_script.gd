1982e4a817726e9be193aaf35c3628b1c9b21fa5
LiamMorrisPro <LiamMorrisPro@gmail.com>
2025-04-16 14:54:24 -0400

    slow progress on dog

--- wild-jam-project/scripts/dog_script.gd
+++ wild-jam-project/scripts/dog_script.gd
@@ -3,13 +3,22 @@ extends RigidBody2D
 
 var detection_array = []
 
+var target_position
+var can_see_enemy : bool = false
+
 var tackle_force = 100.0
 var tackle_direction : Vector2
 
-var can_see_enemy : bool = false
-
 enum state {patrol, chase, tackle}
 var dog_state : state = state.patrol
+var patrol_dir = 1
+
+func _physics_process(delta: float) -> void:
+	
+	if can_see_enemy:
+		#chase player
+		pass
+
 
 func tackle():
 	
@@ -20,8 +29,8 @@ func raycast_detect(target : Player):
 	var query = PhysicsRayQueryParameters2D.create(Vector2(0, 0), target.position, 1|2)
 	var result = space_state.intersect_ray(query)
 	
-	if result.collider:
-		pass
+	if result.collider.is_in_group("Player"):
+		can_see_enemy = true
 
 
 func _on_detection_radius_body_entered(body: Node2D) -> void:
