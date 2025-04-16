bb2fd7ddd5f2231f02d777e8edb7b2c3b79db347
LiamMorrisPro <LiamMorrisPro@gmail.com>
2025-04-16 12:05:00 -0400

    added killzone

---
+++ wild-jam-project/scripts/kill_zone.gd
@@ -0,0 +1,10 @@
+extends Area2D
+class_name KillZone
+
+
+var restart = func (): get_tree().reload_current_scene()
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player") || body.is_in_group("Items"):
+		restart.call_deferred()
