095f07078093e59aa3a181451c12be74f95b0fd3
LiamMorrisPro <LiamMorrisPro@gmail.com>
2025-04-18 17:29:41 -0400

    Added functionality to dog

--- wild-jam-project/scripts/dog_script.gd
+++ wild-jam-project/scripts/dog_script.gd
@@ -13,11 +13,31 @@ enum state {patrol, chase, tackle}
 var dog_state : state = state.patrol
 var patrol_dir = 1
 
+var jump_vector : Vector2 = Vector2(700, 300)
+
 func _physics_process(delta: float) -> void:
+	var target_vector : Vector2 = Vector2.ZERO
 	
-	if can_see_enemy:
-		#chase player
+	
+	for target in detection_array:
+		raycast_detect(target)
+	
+	if detection_array.size() > 0:
+		target_position = detection_array[0].global_position
+		target_vector = target_position - global_position
+	
+	if target_vector.length() > 15:
 		pass
+	
+	#chase enemy
+	if can_see_enemy:
+		if target_vector.x > 0 and target_vector.length() > 20:
+			linear_velocity.x = lerp(linear_velocity.x, 1000.0, delta)
+		elif target_vector.x < -0 and target_vector.length() > 20:
+			linear_velocity.x = lerp(linear_velocity.x, -1000.0, delta)
+	else:
+		linear_velocity.x = 0
+
 
 
 func tackle():
@@ -34,7 +54,9 @@ func raycast_detect(target : Player):
 
 
 func _on_detection_radius_body_entered(body: Node2D) -> void:
-	detection_array.append(body)
+	if body.is_in_group("Player"):
+		detection_array.append(body)
 
 func _on_detection_radius_body_exited(body: Node2D) -> void:
-	detection_array.erase(body)
+	if body.is_in_group("Player"):
+		detection_array.erase(body)
